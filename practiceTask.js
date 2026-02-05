d697418808bec12040844e193e4f42f0a7165b5d
shanto <shantohasibulhasan379@gmail.com>
2026-02-06 03:24:28 +0600

    added practiceTask

--- practiceTask.js
+++ practiceTask.js
@@ -1,6 +1,21 @@
-var iHave = 100;
+var iHave = 1000;
 var orange1Kg = 300;
 var apples1Kg = 400;
 var total = orange1Kg + apples1Kg
-var returnMone -= total
-console.log(returnMoney)
+var returnMoney = iHave - total
+// console.log(returnMoney)
+
+var mathematics = 75.52
+var biology = 65
+var chemistry = 80 
+var physics = 35.45
+var bangla = 99.50
+var result = mathematics + biology + chemistry + physics + bangla
+// console.log(result)
+var average = result / 5
+var finalAverage = average.toFixed(2)
+// console.log(finalAverage)
+
+var number = 119
+var results = number % 5
+console.log(results)
